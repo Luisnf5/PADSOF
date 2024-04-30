a0a26733523ebc747af9bcd1b439691849986f47
ankgale <150606827+ankgale@users.noreply.github.com>
2024-04-30 19:37:42 +0200

    Update VistaPerfil.java

--- src/vistasUsers/VistaPerfil.java
+++ src/vistasUsers/VistaPerfil.java
@@ -5,12 +5,16 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
+import java.time.format.DateTimeFormatter;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SpringLayout;
 
+import system.ArtGallery;
+import users.User;
 import vistasSystem.VistaSystem;
 
 public class VistaPerfil extends JPanel{
@@ -20,7 +24,10 @@ private VistaSystem parent;
 	private JButton notificaciones;
 	private JButton buscar;
 	private JButton principal;
-
+	private JButton datos;
+	private JButton contra;
+	private JButton cerrar;
+	
 	
 	public VistaPerfil(VistaSystem parent) {
 		super();
@@ -59,6 +66,30 @@ private VistaSystem parent;
 		this.add(buscar);
 		
 		this.setPreferredSize(Toolkit.getDefaultToolkit().getScreenSize());
+		
+		this.datos = new JButton("Datos Personales");
+		this.datos.setPreferredSize(new Dimension(150, 30));
+		
+		this.add(this.datos);
+		
+		layout.putConstraint(SpringLayout.NORTH, datos, 70, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.EAST, datos, -50, SpringLayout.EAST, this);
+		
+		this.contra = new JButton("Cambiar Contraseña");
+		this.contra.setPreferredSize(new Dimension(150, 30));
+		
+		this.add(this.contra);
+		
+		layout.putConstraint(SpringLayout.NORTH, contra, 110, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.EAST, contra, -50, SpringLayout.EAST, this);
+		
+		this.cerrar = new JButton("Cerrar Sesion");
+		this.cerrar.setPreferredSize(new Dimension(150, 30));
+		
+		this.add(this.cerrar);
+		
+		layout.putConstraint(SpringLayout.NORTH, cerrar, 150, SpringLayout.NORTH, this);
+		layout.putConstraint(SpringLayout.EAST, cerrar, -50, SpringLayout.EAST, this);
 	}
 	
 	public void setControlador(ActionListener c) {
@@ -66,5 +97,22 @@ private VistaSystem parent;
 		notificaciones.addActionListener(c);
 		buscar.addActionListener(c);
 		principal.addActionListener(c);
+		datos.addActionListener(c);
+		contra.addActionListener(c);
+		cerrar.addActionListener(c);
+	}
+	
+	public void showDatos(User u) {
+		if(u == null)
+			return;
+		
+		JLabel nombre = new JLabel("Nombre: " + u.getName());
+		JLabel apellido = new JLabel("Apellido: " + u.getSurname());
+		JLabel nif = new JLabel("NIF: " + u.getNif());
+		JLabel genero = new JLabel("Genero: " + u.getGender());
+		JLabel fecha = new JLabel("Fecha de Nacimiento: " + u.getBirthDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		
+		
+		
 	}
 }
