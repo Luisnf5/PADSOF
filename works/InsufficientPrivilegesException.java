cbeabff4de027d09f2cc138a43d76ca05e161d57
ankgale <150606827+ankgale@users.noreply.github.com>
2024-06-17 17:50:13 +0200

    Delete works directory

--- works/InsufficientPrivilegesException.java
+++
@@ -1,7 +0,0 @@
-package works;
-
-public class InsufficientPrivilegesException extends RuntimeException{
-	public InsufficientPrivilegesException() {
-		super("No se poseen privilegios de empleado");
-	}
-}
--- works/StaffPrivilegesException.java
+++
@@ -1,7 +0,0 @@
-package works;
-
-public class StaffPrivilegesException extends RuntimeException {
-	public StaffPrivilegesException(String str) {
-		super(str);
-	}
-}
